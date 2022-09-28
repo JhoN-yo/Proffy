9c232c4ad66bbb6891b73813a45fa394c9ca28f2
dev-jhon-ss <dev.jhon.ss@gmail.com>
2022-09-28 16:59:34 -0300

    feat: :sparkles: create structure and styling
    create structure and styling page

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Landing } from './pages/Landing/index.tsx';
+import './assets/styles/global.css';
+
+function App() {
+  return (
+    <Landing />
+  );
+}
+
+export default App;
